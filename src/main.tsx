0f4ba144f2199173ebd415d383b63c442fe86213
unknown <sogand.aghahoseini@gmail.com>
2024-08-20 04:45:08 -0700

    first commit

---
+++ src/main.tsx
@@ -0,0 +1,16 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { CartProvider } from "./context/CartProvider.tsx";
+import { ProductsProvider } from "./context/ProductsProvider.tsx";
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <ProductsProvider>
+      <CartProvider>
+        <App />
+      </CartProvider>
+    </ProductsProvider>
+  </StrictMode>
+);
