4608aa65cee563a6022a0c1ad140338062c6e12a
unknown <sogand.aghahoseini@gmail.com>
2024-12-09 02:59:34 -0800

    Updated file

--- src/context/ProductsProvider.tsx
+++ src/context/ProductsProvider.tsx
@@ -32,7 +32,7 @@ const ProductsContext = createContext<UseProductsContextType>(initContextState);
 type ChildrenType = { children?: ReactElement | ReactElement[] };
 
 export const ProductsProvider = ({ children }: ChildrenType): ReactElement => {
-  const [products, setProducts] = useState<ProductType[]>(initState);
+  const [products, _setProducts] = useState<ProductType[]>(initState);
   // useEffect(() => {
   //   const fetchProducts = async (): Promise<ProductType[]> => {
   //     const data = await fetch("http://localhost:3500/products")
